cf4acebadcae7b2a3a3bd409fc402fc07a05f94f
n0pj <admin@n0pj.com>
2022-06-08 17:45:37 +0900

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::{App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("nbot")
+        .version("0.1.0")
+        .author("n0pj")
+        .about("nbot is a scraping bot")
+        .subcommand(SubCommand::with_name("twitter"));
+
+    let matches = app.get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("twitter") {
+        println!("twitter");
+    }
 }
